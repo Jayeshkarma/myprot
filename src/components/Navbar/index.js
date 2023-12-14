3563c884dd304b0fbfa3b00640b4cb1d22f3fb09
jayesh karma <40052333+Jayeshkarma@users.noreply.github.com>
2023-12-14 18:58:03 +0000

    updated all data

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -26,8 +26,9 @@ const Navbar = () => {
           <NavLink href="#about">About</NavLink>
           <NavLink href='#skills'>Skills</NavLink>
           <NavLink href='#experience'>Experience</NavLink>
-          <NavLink href='#projects'>Projects</NavLink>
+          <NavLink href='https://jayeshkarma.wordpress.com/blog/'>Blogs</NavLink>
           <NavLink href='#education'>Education</NavLink>
+          <NavLink href='https://www.youtube.com/channel/UCeRjNyobwpojbBV15T-d4SQ'>Channel</NavLink>
         </NavItems>
         <ButtonContainer>
           <GitHubButton href={Bio.github} target="_blank">Github Profile</GitHubButton>
@@ -44,12 +45,15 @@ const Navbar = () => {
             <MobileLink href='#experience' onClick={() => {
               setIsOpen(!isOpen)
             }}>Experience</MobileLink>
-            <MobileLink href='#projects' onClick={() => {
+            <MobileLink href='https://jayeshkarma.wordpress.com/blog/' onClick={() => {
               setIsOpen(!isOpen)
-            }}>Projects</MobileLink>
+            }}>Blogs</MobileLink>
             <MobileLink href='#education' onClick={() => {
               setIsOpen(!isOpen)
             }}>Education</MobileLink>
+             <MobileLink href='https://www.youtube.com/channel/UCeRjNyobwpojbBV15T-d4SQ' onClick={() => {
+              setIsOpen(!isOpen)
+            }}>Channel</MobileLink>
             <GitHubButton style={{padding: '10px 16px',background: `${theme.primary}`, color: 'white',width: 'max-content'}} href={Bio.github} target="_blank">Github Profile</GitHubButton>
           </MobileMenu>
         }
